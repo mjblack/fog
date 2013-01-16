mjblack/fog

commit e8630a00831cc17d61d48af9501773002dccc165

[vsphere|compute] Updates reference to service

lib/fog/vsphere/requests/compute/get_virtual_machine.rb
@@ -12,7 +12,7 @@ module Fog
           vm = case id
                  # UUID based
                  when /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/
-                   @connection.searchIndex.FindByUuid :uuid => id, :vmSearch => true, :instanceUuid => true, :datacenter => dc
+                   @service.searchIndex.FindByUuid :uuid => id, :vmSearch => true, :instanceUuid => true, :datacenter => dc
                  else
                    # try to find based on VM name
                    if dc

lib/fog/vsphere/models/compute/cluster.rb
@@ -13,8 +13,8 @@ module Fog
         attribute :overall_status
 
         def resource_pools(filters = { })
-          self.attributes[:resource_pools] ||= id.nil? ? [] : connection.resource_pools({
-                                                                                          :connection => connection,
+          self.attributes[:resource_pools] ||= id.nil? ? [] : service.resource_pools({
+                                                                                          :service => service,
                                                                                           :cluster    => name,
                                                                                           :datacenter => datacenter
                                                                                         }.merge(filters))

lib/fog/vsphere/models/compute/clusters.rb
@@ -12,12 +12,12 @@ module Fog
 
         def all(filters = {})
           requires :datacenter
-          load connection.list_clusters(filters.merge(:datacenter => datacenter))
+          load service.list_clusters(filters.merge(:datacenter => datacenter))
         end
 
         def get(id)
           requires :datacenter
-          new connection.get_cluster(id, datacenter)
+          new service.get_cluster(id, datacenter)
         end
 
       end

lib/fog/vsphere/models/compute/datacenter.rb
@@ -9,23 +9,23 @@ module Fog
         attribute :status
 
         def clusters filters = { }
-          connection.clusters({ :datacenter => name }.merge(filters))
+          service.clusters({ :datacenter => name }.merge(filters))
         end
 
         def networks filters = { }
-          connection.networks({ :datacenter => name }.merge(filters))
+          service.networks({ :datacenter => name }.merge(filters))
         end
 
         def datastores filters = { }
-          connection.datastores({ :datacenter => name }.merge(filters))
+          service.datastores({ :datacenter => name }.merge(filters))
         end
 
         def vm_folders filters = { }
-          connection.folders({ :datacenter => name, :type => :vm }.merge(filters))
+          service.folders({ :datacenter => name, :type => :vm }.merge(filters))
         end
-        
+
         def virtual_machines filters = {}
-          connection.servers({ :datacenter => name }.merge(filters))
+          service.servers({ :datacenter => name }.merge(filters))
         end
 
         def to_s

lib/fog/vsphere/models/compute/datacenters.rb
@@ -10,11 +10,11 @@ module Fog
         model Fog::Compute::Vsphere::Datacenter
 
         def all(filters = {})
-          load connection.list_datacenters(filters)
+          load service.list_datacenters(filters)
         end
 
         def get(name)
-          new connection.get_datacenter(name)
+          new service.get_datacenter(name)
         end
 
       end

lib/fog/vsphere/models/compute/datastores.rb
@@ -11,12 +11,12 @@ module Fog
         attr_accessor :datacenter
 
         def all(filters = {})
-          load connection.list_datastores(filters.merge(:datacenter => datacenter))
+          load service.list_datastores(filters.merge(:datacenter => datacenter))
         end
 
         def get(id)
           requires :datacenter
-          new connection.get_datastore(id, datacenter)
+          new service.get_datastore(id, datacenter)
         end
 
       end

lib/fog/vsphere/models/compute/folder.rb
@@ -14,7 +14,7 @@ module Fog
 
         def vms
           return [] if type.to_s != 'vm'
-          connection.servers(:folder => path, :datacenter => datacenter)
+          service.servers(:folder => path, :datacenter => datacenter)
         end
 
         def to_s

lib/fog/vsphere/models/compute/folders.rb
@@ -13,12 +13,12 @@ module Fog
         def all(filters = { })
           requires :datacenter
           requires :type
-          load connection.list_folders(filters.merge(:datacenter => datacenter, :type => type, :path => path))
+          load service.list_folders(filters.merge(:datacenter => datacenter, :type => type, :path => path))
         end
 
         def get(id)
           requires :datacenter
-          new connection.get_folder(id, datacenter, type)
+          new service.get_folder(id, datacenter, type)
         end
 
       end

commit 0cfd5efdb297cef41f82abf46d25220790d2772a

[vsphere] fixed an issue introduced in commit e8630a0 where @service is nil

lib/fog/vsphere/requests/compute/get_virtual_machine.rb
@@ -12,7 +12,7 @@ module Fog
           vm = case id
                  # UUID based
                  when /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/
-                   @service.searchIndex.FindByUuid :uuid => id, :vmSearch => true, :instanceUuid => true, :datacenter => dc
+                   @connection.searchIndex.FindByUuid :uuid => id, :vmSearch => true, :instanceUuid => true, :datacenter => dc
                  else
                    # try to find based on VM name
                    if dc